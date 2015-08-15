3e4fe6d82fc17581bb4c8925310dc71b0513b524
Paul Olguin <prometheus@silentflea.com>
2015-08-14 21:45:19 -0500

    Initial commit

---
+++ api/controllers/DreamController.js
@@ -0,0 +1,11 @@
+/**
+ * DreamController
+ *
+ * @description :: Server-side logic for managing dreams
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
